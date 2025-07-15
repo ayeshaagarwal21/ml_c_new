2c6ff3f2998aa96ea61a71aa9a723688c4882b78
Ayesha Agarwal <ayeshaagarwal433@gmail.com>
2025-07-15 12:22:23 +0530

    added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from .env import load_.env
-load_.env() 
+from dotenv import load_dotenv
+load_dotenv() 
 import streamlit as st
 import os
 import google.generativeai as genai
@@ -10,7 +10,7 @@ model=genai.GenerativeModel("gemini-2.0-flash")
 
 def my_output(query):
     response=model.generate_content(query)
-    return response.txt
+    return response.text
 
 st.set_page_config(page_title="chat_bot")
 st.header("chat_bot")
